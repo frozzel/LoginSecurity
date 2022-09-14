c5a872ad7f5c616d2e6b4d60f5802aa3aa54d84c
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-09-14 11:21:27 -0400

    added characters arrays

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Global Varibles/////////////////
+///////////////////////////////////////////Global Varibles/////////////////
 var  generatePassword
 var PassLength
 var Numbers
@@ -15,8 +15,14 @@ console.log(UpperCase)
 console.log(LowCase)
 console.log(SymbolsCont)
 
+////// ///////////////////////////////////Character Arrays/////////////////
 
-//Funtion//////////////////
+SymbolsArray =["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
+NumbArray =[0,1, 2, 3, 4, 5, 6, 7, 8, 9];
+UpArray =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+LowArray =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+//////////////////////////////////////////////Funtion//////////////////
 function generatePassword(){
   PassLength = prompt('How long do you want your password? 8-128')
   if (PassLength < 7 || PassLength > 128) {
